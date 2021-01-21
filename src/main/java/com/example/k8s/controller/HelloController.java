da4affc11dd650ec35aede0df9eeb0ac10bf5e31
bingh <412337373@qq.com>
2021-01-21 14:48:36 +0800

    commit

--- src/main/java/com/example/k8s/controller/HelloController.java
+++ src/main/java/com/example/k8s/controller/HelloController.java
@@ -1,14 +1,19 @@
 package com.example.k8s.controller;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    private static Logger logger = LoggerFactory.getLogger(HelloController.class);
+
     @GetMapping("/")
     public String sayHello() {
+        logger.info("hello world");
         return "hello, world";
     }
 }
