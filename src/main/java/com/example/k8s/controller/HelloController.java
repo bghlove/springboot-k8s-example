c95d04bb0e463f560eef6caa20e7a972f43ee37e
bingh <412337373@qq.com>
2021-01-21 16:43:14 +0800

    commit

--- src/main/java/com/example/k8s/controller/HelloController.java
+++ src/main/java/com/example/k8s/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.k8s.controller;
 
 
+import cn.hutool.core.date.DateUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +14,7 @@ public class HelloController {
 
     @GetMapping("/")
     public String sayHello() {
-        logger.info("hello world");
+        logger.info("hello world---->"+ DateUtil.now());
         return "hello, world";
     }
 }
