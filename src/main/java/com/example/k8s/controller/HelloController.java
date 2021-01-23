3d6a6efca1f1343f3567781038be6b34d83fa3ad
bingh <412337373@qq.com>
2021-01-23 21:18:54 +0800

    logs

--- src/main/java/com/example/k8s/controller/HelloController.java
+++ src/main/java/com/example/k8s/controller/HelloController.java
@@ -2,10 +2,13 @@ package com.example.k8s.controller;
 
 
 import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.io.file.FileWriter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
 
 @RestController
 public class HelloController {
@@ -15,6 +18,9 @@ public class HelloController {
     @GetMapping("/")
     public String sayHello() {
         logger.info("hello world---->"+ DateUtil.now());
+        File file=new File("d:/logs/spring.log");
+        FileWriter fileWriter=new FileWriter(file);
+        fileWriter.append("hello world---->"+ DateUtil.now());
         return "hello, world";
     }
 }
